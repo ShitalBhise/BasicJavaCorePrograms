953cb8b83b1e6090be4f3b7a87fedfafabd949ad
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-19 22:53:33 +0530

    Added: Distance

---
+++ src/com/java/Distance.java
@@ -0,0 +1,25 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Distance {
+	public static double distance(double x, double y) {
+		double distance = 0;
+		distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		return distance;
+	}
+
+	public static void main(String[] args) {
+		double x = 0;
+		double y = 0;
+		System.out.println("Enter the x and y co ordinates");
+		Scanner scanner = new Scanner(System.in);
+		x = scanner.nextDouble();
+		y = scanner.nextDouble();
+		double distance = distance(x, y);
+		System.out.println("distance = " + distance);
+		scanner.close();
+
+	}
+
+}
